cd3a6c9d87bcbc18d61999732e76fbfdc36b37a6
Mark Zwart <mark.zwart@pobox.com>
2024-02-06 10:14:11 +0100

    Added documentation

--- controller/routers/discogs.py
+++ controller/routers/discogs.py
@@ -26,6 +26,8 @@ class Artist(BaseModel):
 
 @router.get("/check-credentials/")
 async def check_user_credentials():
+    """ Check if user credentials for Discogs are present
+    """
     if discogs.check_user_tokens():
         return{'description': 'All OK!'}
     else:
@@ -33,16 +35,25 @@ async def check_user_credentials():
 
 @router.get("/get-user-access/")
 async def open_discogs_permissions_page():
-    result = discogs.request_user_access(callback_url=f"http://localhost:{config['PORT_CONTROLLER']}/discogs/receive-token/")
+    """ Asks user to give app access to Discogs account, with a callback url to handle validation
+    """
+    callback_url = f"http://localhost:{config['PORT_CONTROLLER']}/discogs/receive-token/"
+    result = discogs.request_user_access(callback_url=callback_url)
     return result
 
 @router.get("/receive-token/")
 async def accept_user_token(oauth_token: str, oauth_verifier: str):
+    """Callback function to process the user authentication result
+    """
     result = discogs.save_user_token(oauth_verifier)
     return result
 
 @router.get("/artists-image/")
 async def get_artists_image(name_artist: str):
+    """ Retrieve an image for the artist
+
+    - **name_artist**: Name of the artist search, matching is fuzzy....
+    """
     result = discogs.get_artist_image(name_artist)
     if result['status'] == 200:
         headers = {"Content-Type": 'image/jpeg'}
@@ -53,4 +64,6 @@ async def get_artists_image(name_artist: str):
 
 @router.post("/bulk-artist-images/")
 async def import_discogs_artist_images(list_artists: List[Artist]):
+    """Imperfect function for populating the user images cache.... Not recommended for non-developers.
+    """
     discogs.artist_images_bulk(list_artists)
