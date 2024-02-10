14403153cee5d817ac245e8cfb18aabca6553412
Mark Zwart <mark.zwart@pobox.com>
2024-02-10 22:51:56 +0100

    Added top period

--- controller/lastfm/lastfm.py
+++ controller/lastfm/lastfm.py
@@ -4,7 +4,6 @@ from dateutil import parser
 
 import logging
 import os
-import webbrowser
 import time
 
 from utils import SecretsYAML
@@ -15,46 +14,57 @@ config = {
 }
 
 logging.basicConfig(
-    format='%(levelname)s:\t%(asctime)s - %(module)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S',
-    level=logging.INFO
+    format="%(levelname)s:\t%(asctime)s - %(module)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO,
 )
 logger = logging.getLogger(__name__)
 
+
 class LastFm:
     def __init__(self, host: str, port: int) -> None:
-        self._secrets = {'session_key': '', 'username': ''}
+        self._secrets = {"session_key": "", "username": ""}
         self._user_secrets_file = SecretsYAML(
-            file_path='config/secrets.yml',
-            app='lastfm',
-            expected_keys=set(self._secrets.keys())
-            )
-        self._api_key = '2e031e7b2ab98c1bce14436016185a4d'
-        self._api_secret = 'fcce22fac65bcdcfcc249841f4ffed8b'
-        self._network = pylast.LastFMNetwork(api_key=self._api_key,
-                                             api_secret=self._api_secret)
+            file_path="config/secrets.yml",
+            app="lastfm",
+            expected_keys=set(self._secrets.keys()),
+        )
+        self._api_key = "2e031e7b2ab98c1bce14436016185a4d"
+        self._api_secret = "fcce22fac65bcdcfcc249841f4ffed8b"
+
+        self._network = pylast.LastFMNetwork(
+            api_key=self._api_key, api_secret=self._api_secret
+        )
         self._session_key_gen = pylast.SessionKeyGenerator(self._network)
+
         self.check_user_data()
 
     def check_user_data(self):
         result = self._user_secrets_file.read_secrets()
         if result is not None:
-            logger.info("Found username and session_key in config file config/secrets.yml")
-            self._network.session_key = self._session_key = result['session_key']
-            self._network.username = self._username = result['username']
+            logger.info(
+                "Found username and session_key in config file config/secrets.yml"
+            )
+            self._network.session_key = self._session_key = result["session_key"]
+            self._network.username = self._username = result["username"]
         else:
-            logger.warning("No entry found of username and session_key in config file config/secrets.yml")
-            self._session_key :str = None
-            self._username :str = None
+            logger.warning(
+                "No entry found of username and session_key in config file config/secrets.yml"
+            )
+            self._session_key: str = None
+            self._username: str = None
             return False
         return True
 
     def request_user_access(self, username: str) -> dict:
-        """ Prompt your user to "accept" the terms of your application. The application
-            will act on behalf of their discogs.com account."""
+        """Prompt your user to "accept" the terms of your application. The application
+        will act on behalf of their discogs.com account."""
         self._username = username
         url = self._session_key_gen.get_web_auth_url()
-        return {'message': 'Authorize BoelMuziek for access to your Last.fm account :',
-                'url': url}
+        return {
+            "message": "Authorize BoelMuziek for access to your Last.fm account :",
+            "url": url,
+        }
 
     def await_authorization(self, url):
         while True:
@@ -63,17 +73,17 @@ class LastFm:
                 break
             except pylast.WSError:
                 time.sleep(1)
-        self._network.session_key = self._secrets['session_key'] = session_key
-        self._network.username = self._secrets['username'] = self._username
+        self._network.session_key = self._secrets["session_key"] = session_key
+        self._network.username = self._secrets["username"] = self._username
         self._user_secrets_file.write_secrets(dict_secrets=self._secrets)
-        return {'key': session_key}
+        return {"key": session_key}
 
     def save_session_key(self, session_key: str, username: str) -> dict:
-        logger.info("Receiving confirmation of access to the user\'s Last.fm account")
-        self._network.session_key = self._secrets['session_key'] = session_key
-        self._network.username = self._secrets['username'] = username
+        logger.info("Receiving confirmation of access to the user's Last.fm account")
+        self._network.session_key = self._secrets["session_key"] = session_key
+        self._network.username = self._secrets["username"] = username
         self._user_secrets_file.write_secrets(dict_secrets=self._secrets)
-        return {'status_code': 200, 'message': f"User connected."}
+        return {"status_code": 200, "message": "User connected."}
 
     def get_artist_bio(self, name_artist: str) -> str:
         logger.info(f"Retrieving artist bio of {name_artist}")
@@ -86,20 +96,32 @@ class LastFm:
             bio = None
         return bio
 
-    def scrobble_track(self, name_artist: str, name_song: str, name_album: str=None) -> None:
+    def scrobble_track(
+        self, name_artist: str, name_song: str, name_album: str = None
+    ) -> None:
         now = int(time.time())
         logger.info(f"Scrobbling {name_artist}-{name_song} to Last.fm")
         try:
-            self._network.scrobble(artist=name_artist, title=name_song, album=name_album, timestamp=now)
+            self._network.scrobble(
+                artist=name_artist, title=name_song, album=name_album, timestamp=now
+            )
         except pylast.NetworkError as e:
-            logger.error(f"Failed to scrobble \'{name_artist}-{name_song}\' due to {e.underlying_error}")
+            logger.error(
+                f"Failed to scrobble '{name_artist}-{name_song}' due to {e.underlying_error}"
+            )
 
-    def now_playing_track(self, name_artist: str, name_song: str, name_album: str=None) -> None:
+    def now_playing_track(
+        self, name_artist: str, name_song: str, name_album: str = None
+    ) -> None:
         logger.info(f"Set 'now playing' {name_artist}-{name_song} to Last.fm")
         try:
-            self._network.update_now_playing(artist=name_artist, title=name_song, album=name_album)
+            self._network.update_now_playing(
+                artist=name_artist, title=name_song, album=name_album
+            )
         except pylast.NetworkError as e:
-            logger.error(f"Failed to set Now Playing for \'{name_artist}-{name_song}\' due to {e.underlying_error}")
+            logger.error(
+                f"Failed to set Now Playing for '{name_artist}-{name_song}' due to {e.underlying_error}"
+            )
 
     def love_track(self, name_artist: str, name_song: str) -> bool:
         logger.info(f"Loving {name_artist}-{name_song} on Last.fm")
@@ -108,10 +130,14 @@ class LastFm:
             track.love()
             return True
         except pylast.NetworkError as e:
-            logger.error(f"Failed to set love for \'{name_artist}-{name_song}\' due to {e.underlying_error}")
+            logger.error(
+                f"Failed to set love for '{name_artist} - {name_song}' due to {e.underlying_error}"
+            )
             return False
-        except pylast.WSError as e:
-            logger.error(f"Failed to set love for \'{name_artist}-{name_song}\' due to authorization issues, re-authenticate.")
+        except pylast.WSError:
+            logger.error(
+                f"Failed to set love for '{name_artist} - {name_song}' due to authorization issues, re-authenticate."
+            )
             return False
 
     def get_loved_tracks(self, limit: int) -> list:
@@ -122,42 +148,45 @@ class LastFm:
         for track in lst_tracks:
             lst_loved_tracks.append(
                 {
-                    'datetime': parser.parse(track.date),
-                    'timestamp': int(track.timestamp),
-                    'song': track.track.title,
-                    'artist': track.track.artist.name
+                    "datetime": parser.parse(track.date),
+                    "timestamp": int(track.timestamp),
+                    "song": track.track.title,
+                    "artist": track.track.artist.name,
                 }
             )
         return lst_loved_tracks
 
-    def get_top_assets(self, type_asset: str, limit: int) -> dict:
-        # TODO: Add time frames
+    def get_top_assets(
+        self, type_asset: str, limit: int, period: str = "overall"
+    ) -> dict:
         user = self._network.get_user(username=self._username)
         lst_results = []
         try:
-            if type_asset == 'artists':
-                top_items = user.get_top_artists(limit=limit)
-            elif type_asset == 'albums':
-                top_items = user.get_top_albums(limit=limit)
-            elif type_asset == 'songs':
-                top_items = user.get_top_tracks(limit=limit)
+            if type_asset == "artists":
+                top_items = user.get_top_artists(period=period, limit=limit)
+            elif type_asset == "albums":
+                top_items = user.get_top_albums(period=period, limit=limit)
+            elif type_asset == "songs":
+                top_items = user.get_top_tracks(period=period, limit=limit)
         except pylast.NetworkError as e:
             logger.error(f"Failed to get Top Albums because of {e}")
         for top_item in top_items:
-            if type_asset == 'artists':
-                dict_top_item = {'name_artist': top_item.item.name,
-                                 'qty_plays': int(top_item.weight)}
-            elif type_asset == 'albums':
-                dict_top_item = {'name_artist': top_item.item.artist.name,
-                                 'name_album': top_item.item.title,
-                                 'qty_plays': int(top_item.weight)}
-            elif type_asset == 'songs':
-                dict_top_item = {'name_artist': top_item.item.artist.name,
-                                 'name_song': top_item.item.title,
-                                 'qty_plays': int(top_item.weight)}
+            if type_asset == "artists":
+                dict_top_item = {
+                    "name_artist": top_item.item.name,
+                    "qty_plays": int(top_item.weight),
+                }
+            elif type_asset == "albums":
+                dict_top_item = {
+                    "name_artist": top_item.item.artist.name,
+                    "name_album": top_item.item.title,
+                    "qty_plays": int(top_item.weight),
+                }
+            elif type_asset == "songs":
+                dict_top_item = {
+                    "name_artist": top_item.item.artist.name,
+                    "name_song": top_item.item.title,
+                    "qty_plays": int(top_item.weight),
+                }
             lst_results.append(dict_top_item)
         return lst_results
-
-
-
-
--- controller/routers/lastfm.py
+++ controller/routers/lastfm.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, BackgroundTasks, HTTPException
 from dotenv import dotenv_values
-import pandas as pd
 
 from enum import Enum
 import os
@@ -12,83 +11,110 @@ config = {
     **os.environ,  # override loaded values with environment variables
 }
 
-lastfm = LastFm(host=config['HOST_CONTROLLER'], port=config['PORT_CONTROLLER'])
+lastfm = LastFm(host=config["HOST_CONTROLLER"], port=config["PORT_CONTROLLER"])
 
-router = APIRouter(
-    prefix='/lastfm',
-    tags=['Last.fm resources']
-)
+router = APIRouter(prefix="/lastfm", tags=["Last.fm resources"])
+
 
 class TypeAsset(str, Enum):
     artist = "artists"
     album = "albums"
     song = "songs"
 
+
+class PeriodTopPlayed(str, Enum):
+    all_time = "overall"
+    last_7_days = "7day"
+    last_month = "1month"
+    last_3_months = "3month"
+    last_6_months = "6month"
+    last_12_months = "12month"
+
+
 @router.get("/check-credentials/")
 async def check_user_credentials():
-    """ Check if the user already has given the app access to Last.fm
-    """
+    """Check if the user already has given the app access to Last.fm"""
     if lastfm.check_user_data():
-        return{'description': 'All OK!'}
+        return {"description": "All OK!"}
     else:
-        raise HTTPException(status_code=401, detail='Let user (re-)authorize access to her/his Last.fm account')
+        raise HTTPException(
+            status_code=401,
+            detail="Let user (re-)authorize access to her/his Last.fm account",
+        )
+
 
 @router.get("/get-user-access/")
-async def open_lastfm_permissions_page(username: str, background_tasks: BackgroundTasks):
-    """ Launching the Last.fm webpage to request access. Once granted the callback url is used for verification.
+async def open_lastfm_permissions_page(
+    username: str, background_tasks: BackgroundTasks
+):
+    """Launching the Last.fm webpage to request access. Once granted the callback url is used for verification.
 
     - **username**, not strictly necessary for the authorization process, but aids user statistics retrieval
     """
     result = lastfm.request_user_access(username=username)
-    background_tasks.add_task(wait_authorization, result['url'])
+    background_tasks.add_task(wait_authorization, result["url"])
     return result
+
 
 def wait_authorization(url: str):
     lastfm.await_authorization(url)
 
+
 @router.get("/artist/")
 async def get_artist_bio(name_artist: str):
-    """ Retrieving the artist bio
+    """Retrieving the artist bio
 
     - **name_artist** - The artist's name (surprise, surprise)
     """
     result = lastfm.get_artist_bio(name_artist)
     if result is None:
-        raise HTTPException(status_code=401,detail="App not authorized for last.fm or invalid user token")
+        raise HTTPException(
+            status_code=401,
+            detail="App not authorized for last.fm or invalid user token",
+        )
     return result
+
 
 @router.get("/love-song/")
 async def love_track(name_artist: str, name_song: str):
-    """ Setting 'loved' for a song on Last.fm
+    """Setting 'loved' for a song on Last.fm
 
     - **name_artist**: Name of artist
     - **name_song**: Name of the song to set to 'loved'
     """
     is_success = lastfm.love_track(name_artist=name_artist, name_song=name_song)
     if is_success:
-        return { 'details': f"Loved '{name_song}' by {name_artist}"}
+        return {"details": f"Loved '{name_song}' by {name_artist}"}
     else:
         HTTPException(500, "Either network or authorization issue")
 
+
 @router.get("/loved/")
-async def get_loved_songs(limit: int=1000):
-    """  Retrieving loved songs
+async def get_loved_songs(limit: int = 1000):
+    """Retrieving loved songs
 
     - **limit**: The number of assets you want to retrieve, max = 1000
     """
     lst_songs = lastfm.get_loved_tracks(limit=limit)
     return lst_songs
 
+
 @router.get("/top-assets/")
-async def get_top_assets(type_asset: TypeAsset, limit: int=1000):
-    """ Retrieving top scrobbled media assets on Last.fm
+async def get_top_assets(
+    type_asset: TypeAsset,
+    limit: int = 1000,
+    period: PeriodTopPlayed = PeriodTopPlayed.all_time,
+):
+    """Retrieving top scrobbled media assets on Last.fm
 
     - **type_asset**: Type of assets you want to aggregate your scrobbles to
     - **limit**: The number of assets you want to retrieve, max = 1000
     """
-    lst_results = lastfm.get_top_assets(type_asset=type_asset.value,
-                                        limit=limit)
+    lst_results = lastfm.get_top_assets(
+        type_asset=type_asset.value, limit=limit, period=period.value
+    )
     return lst_results
+
 
 """ @router.get("/albums/top")
 async def get_most_played_albums():
@@ -115,4 +141,3 @@ async def get_most_played_albums():
     df_mpd['qty_plays'] = df_mpd['qty_plays'].str
 
     return df_mpd.to_dict('records') """
-
