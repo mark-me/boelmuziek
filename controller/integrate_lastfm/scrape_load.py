a89ebfcd73008b711c0d1ba46eb3b88c313ad5af
Mark Zwart <mark.zwart@pobox.com>
2024-02-11 01:09:22 +0100

    Prototyping

--- controller/integrate_lastfm/scrape_load.py
+++ controller/integrate_lastfm/scrape_load.py
@@ -1,5 +1,7 @@
 from dotenv import dotenv_values
+import pandas as pd
 
+import asyncio
 import logging
 import os
 import sqlite3
@@ -9,7 +11,7 @@ sys.path.insert(1, "/".join(os.path.realpath(__file__).split("/")[0:-2]))
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 os.chdir("..")
 
-from mpd_client.mpd_client import MPDController
+from mpd_client.mpd_library import MPDLibrary
 from lastfm.lastfm import LastFm
 
 config = {
@@ -26,12 +28,28 @@ logger = logging.getLogger(__name__)
 
 # Start main code
 lastfm = LastFm(host=config["HOST_CONTROLLER"], port=config["PORT_CONTROLLER"])
-mpd_client = MPDController(host=config["HOST_MPD"])
+library = MPDLibrary(host=config["HOST_MPD"])
+con_sqlite = sqlite3.connect(':memory:')
 
-file_db = "config/lastfm.sqlite"
-con = sqlite3.connect(file_db)
+async def main():
+    # Get top albums
+    lst_albums = lastfm.get_top_assets('albums', limit=100, period="7day")
+    df_albums = pd.DataFrame.from_records(lst_albums)
+    df_albums.to_sql('albums_lastfm', con_sqlite, if_exists='replace', index=False)
+    print("-- Last.fm")
+    print(df_albums.head())
 
-lastfm = LastFm(host=config["HOST_CONTROLLER"], port=config["PORT_CONTROLLER"])
-mpd_client = MPDController(host=config["HOST_MPD"])
+    lst_albums = await library.get_albums()
+    df_albums = pd.DataFrame.from_records(lst_albums)
+    df_albums.to_sql('albums_mpd', con_sqlite, if_exists='replace', index=False)
+    print("-- MPD")
+    print(df_albums.head())
 
-lastfm.get_top_assets('albums')
+    sql_statement = "SELECT lfm.name_artist, lfm.name_album, lfm.qty_plays FROM albums_lastfm AS lfm INNER JOIN albums_mpd as mpd ON lfm.name_artist = mpd.albumartist AND lfm.name_album = mpd.album"
+    df_albums = pd.read_sql(sql=sql_statement, con=con_sqlite)
+    print("-- Join")
+    print(df_albums.head())
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
