ecf8a6e4fd9eb3518270b2ce67c0f647becb9f90
Mark Zwart <mark.zwart@pobox.com>
2024-02-26 00:47:02 +0100

    First change

--- controller/lastfm/lastfm.py
+++ controller/lastfm/lastfm.py
@@ -1,12 +1,16 @@
-import pylast
-from dotenv import dotenv_values
-from dateutil import parser
 
 import logging
 import os
 import time
+import json
 
-from utils import SecretsYAML
+from dotenv import dotenv_values
+from dateutil import parser
+from lastfmxpy import (
+    api,
+    methods,
+    params,
+)
 
 config = {
     **dotenv_values(".env"),  # load shared development variables
@@ -22,79 +26,20 @@ logger = logging.getLogger(__name__)
 
 
 class LastFm:
-    def __init__(self, host: str, port: int) -> None:
-        self._secrets = {"session_key": "", "username": ""}
-        self._user_secrets_file = SecretsYAML(
-            file_path="config/secrets.yml",
-            app="lastfm",
-            expected_keys=set(self._secrets.keys()),
+    def __init__(self) -> None:
+        self._client = api.LastFMApi(
+            api_key="2e031e7b2ab98c1bce14436016185a4d",
+            shared_secret="fcce22fac65bcdcfcc249841f4ffed8b"
         )
-        self._api_key = "2e031e7b2ab98c1bce14436016185a4d"
-        self._api_secret = "fcce22fac65bcdcfcc249841f4ffed8b"
-
-        self._network = pylast.LastFMNetwork(
-            api_key=self._api_key, api_secret=self._api_secret
-        )
-        self._session_key_gen = pylast.SessionKeyGenerator(self._network)
-
-        self.check_user_data()
-
-    def check_user_data(self):
-        result = self._user_secrets_file.read_secrets()
-        if result is not None:
-            logger.info(
-                "Found username and session_key in config file config/secrets.yml"
-            )
-            self._network.session_key = self._session_key = result["session_key"]
-            self._network.username = self._username = result["username"]
-        else:
-            logger.warning(
-                "No entry found of username and session_key in config file config/secrets.yml"
-            )
-            self._session_key: str = None
-            self._username: str = None
-            return False
-        return True
-
-    def request_user_access(self, username: str) -> dict:
-        """Prompt your user to "accept" the terms of your application. The application
-        will act on behalf of their discogs.com account."""
-        self._username = username
-        url = self._session_key_gen.get_web_auth_url()
-        return {
-            "message": "Authorize BoelMuziek for access to your Last.fm account :",
-            "url": url,
-        }
-
-    def await_authorization(self, url):
-        while True:
-            try:
-                session_key = self._session_key_gen.get_web_auth_session_key(url)
-                break
-            except pylast.WSError:
-                time.sleep(1)
-        self._network.session_key = self._secrets["session_key"] = session_key
-        self._network.username = self._secrets["username"] = self._username
-        self._user_secrets_file.write_secrets(dict_secrets=self._secrets)
-        return {"key": session_key}
-
-    def save_session_key(self, session_key: str, username: str) -> dict:
-        logger.info("Receiving confirmation of access to the user's Last.fm account")
-        self._network.session_key = self._secrets["session_key"] = session_key
-        self._network.username = self._secrets["username"] = username
-        self._user_secrets_file.write_secrets(dict_secrets=self._secrets)
-        return {"status_code": 200, "message": "User connected."}
 
     def get_artist_bio(self, name_artist: str) -> str:
-        logger.info(f"Retrieving artist bio of {name_artist}")
-        print(self._network.session_key)
-        artist = self._network.get_artist(artist_name=name_artist)
-        try:
-            bio = artist.get_bio_content()
-        except pylast.WSError as e:
-            print(e)
-            bio = None
-        return bio
+        response: str = self._client.post(
+            method=methods.Artist.GET_INFO,
+            params=params.ArtistGetInfo(artist=name_artist),
+            additional_params=dict(format="json")
+        )
+        artist = json.loads(response)['artist']
+        return artist["bio"]["content"]
 
     def scrobble_track(
         self, name_artist: str, name_song: str, name_album: str = None
@@ -191,3 +136,8 @@ class LastFm:
                 }
             lst_results.append(dict_top_item)
         return lst_results
+
+if __name__ == "__main__":
+    lstfm = LastFm()
+    bio = lstfm.get_artist_bio(name_artist="Nick Cave")
+    print(bio)
